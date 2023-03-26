07fd2126d8245e763f8d2112bbb4cad57137f99c
xamwise <64870626+xamwise@users.noreply.github.com>
2023-03-27 00:22:41 +0200

    highLR

--- train_partseg_hydra.py
+++ train_partseg_hydra.py
@@ -291,7 +291,7 @@ def main(args):
             epoch + 1, test_metrics['accuracy'], test_metrics['average_iou'], test_metrics['average_acc']))
         if (test_metrics['average_iou'] >= best_avg_iou):
             logger.info('Save model...')
-            savepath = f'best_models/best_model_own_data_pointnet2_{str(args.num_point)}.pth'
+            savepath = f'best_models/best_model_own_data_highLR_{str(args.num_point)}.pth'
             logger.info('Saving at %s' % savepath)
             state = {
                 'epoch': epoch,
